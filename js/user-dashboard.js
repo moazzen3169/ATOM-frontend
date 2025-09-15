dc30a0076b6f9583ecf55ad1961a20beb61277da
hadi3169 <moazzenhadi46@gmail.com>
2025-09-15 18:31:26 +0330

    complate the html css and design for usera dashboard

--- js/user-dashboard.js
+++ js/user-dashboard.js
@@ -11,3 +11,36 @@ editUserBtn.addEventListener("click", function() {
 closeBtn.addEventListener("click", function() {
     userInfo_editForm.classList.remove("show");
 });
+
+
+
+    // سایدبار رو لود کن
+    fetch("user-sidebar.html")
+      .then(res => res.text())
+      .then(data => {
+        document.getElementById("sidebar").innerHTML = data;
+      });
+
+
+
+
+
+    //   کد های سایدبار مخفی
+
+
+
+    function openSidebar() {
+        document.getElementById("rightSidebar").classList.add("active");
+      }
+  
+      function closeSidebar() {
+        document.getElementById("rightSidebar").classList.remove("active");
+      }
+  
+      // بستن با کلیک روی overlay
+      document.getElementById("rightSidebar").addEventListener("click", function(e) {
+        if (e.target.id === "rightSidebar") {
+          closeSidebar();
+        }
+          });ent.querySelector(".close_btn").addEventListener("click", closeSidebar);
+  
