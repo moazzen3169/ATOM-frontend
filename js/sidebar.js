843782ddea3061e788248ababd51888a533d88c4
hadi3169 <moazzenhadi46@gmail.com>
2025-09-28 16:10:30 +0330

    update sidebar codes

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,16 +1,3 @@
-const editUserBtn = document.querySelector(".edit_user_btn");
-const closeBtn = document.querySelector(".close_user_btn");
-const userInfo_editForm = document.querySelector(".user_info_edit_form");
-
-editUserBtn.addEventListener("click", function() {
-    userInfo_editForm.classList.add("show");
-});
-
-closeBtn.addEventListener("click", function() {
-    userInfo_editForm.classList.remove("show");
-});
-
-
 
 // سایدبار رو لود کن
 const sidebarElement = document.getElementById("sidebar");
